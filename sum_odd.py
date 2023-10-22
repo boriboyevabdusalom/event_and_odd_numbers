4160deed9543e7597dc0099c48aeed01c1bb8179
abdusalom <boriboyevabdusalom@gmail.com>
2023-10-22 17:22:15 +0500

    trf

--- sum_odd.py
+++ sum_odd.py
@@ -1,6 +1,7 @@
 #A four-digit integer is given. Find the sum of odd digits in it.
 #Create a variable "var_int" and assign it a four-digit integer value.
-
+var_int = 3456
 #Create a variable "sum_odd" and assign it 0.
-
+sum_even = 0
 #Find the sum of the odd digits in the variable "var_int".
+print(var_int//10%10+var_int//1000)
